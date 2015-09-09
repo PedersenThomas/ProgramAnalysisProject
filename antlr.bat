e930927cc6faf53776b5f8f4cb276b8d425e0605
Thomas Pedersen <pedersenthomas@outlook.com>
2015-09-10 01:13:22 +0200

    Added files given by Hanne
    On campusnet is the there a parser.zip.
    I removed all the SVN and mac files.

---
+++ antlr.bat
@@ -0,0 +1,3 @@
+java -jar "%~dp0antlr-3.4-complete.jar" .\src\thelang\TheLang.g
+REM javac -sourcepath .\src -cp .\*.jar .\src\thelang\Main.java -d bin
+REM java -cp .\antlr-3.5-complete.jar;.\bin thelang.Main %1
