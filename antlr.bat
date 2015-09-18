cbf0a1331ca5debf59c3f847963d1b877cf8a768
Thomas Pedersen <pedersenthomas@outlook.com>
2015-09-18 23:50:38 +0200

    Cleaned the antlr.bat file.

--- antlr.bat
+++ antlr.bat
@@ -1,5 +1,1 @@
 java -jar "%~dp0antlr-3.4-complete.jar" .\src\thelang\TheLang.g
-::java -jar "%~dp0antlr-3.4-complete.jar" .\src\thelang\TheLangWalker.g
-
-:: javac -sourcepath .\src -cp .\*.jar .\src\thelang\Main.java -d bin
-:: java -cp .\antlr-3.5-complete.jar;.\bin thelang.Main %1
